a844471b1fff30b4c4d7dce1b1d7c7dd76c290d2
wzc <958778358@qq.com>
2024-03-12 23:39:27 +0800

    initial structure

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,30 @@
+package com.sky.mapper;
+
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * mybatis注解
+     * 根据用户名查询员工，并添加到创建的对象中
+     * @param username
+     * @return
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+    /**
+     * 新建员工
+     * @param employee
+     */
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
+            "values" +
+            "(#{name},#{username},#{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})"
+    )
+    void InsertUser(Employee employee);
+
+}
---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+    /**
+     * 添加新员工
+     * @param employeeLoginDTO
+     * @return
+     */
+    void save(EmployeeDTO employeeLoginDTO);
+
+}
